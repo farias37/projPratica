0cbf74bf072c42794b039676177c6db8f8ce329e
farias37 <gabffarias@gmail.com>
2016-09-30 08:44:07 -0300

    Add files via upload

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
+
 function barraMenu()
 {
+    //quando desce o scroll
     document.getElementById("head").style.height = "10%";
 //    document.getElementById("logo").style.backgroundImage = "url('logo-pd15.png')";
     document.getElementById("logo").style.height = "10%";
@@ -8,9 +10,44 @@ function barraMenu()
     document.getElementById("head-vermelho").style.height = "0%";
     document.getElementById("head-branco").style.height = "0%";
     document.getElementById("nome").style.left = "-500px";
-    document.getElementById("nome").style.fontSize = "30px";
-    document.getElementById("nome").style.fontSize = "30px";
+    //document.getElementById("nome").style.fontSize = "30px";
+    document.getElementById("nome").style.fontSize = "0px";
+    document.getElementById("nome").style.height = "0%";
     
+    document.getElementById("menu").style.height = "10%";
+    document.getElementById("menu").style.top = "0%";
+    document.getElementById("menu").style.width = "90%";
+    document.getElementById("menu").style.left = "10%";
+    
+    document.getElementById("menu-login").style.left = "10%";
+    document.getElementById("menu-login").style.height = "10%";
+    document.getElementById("menu-login").style.top = "0%";
+    
+    document.getElementById("menu-loja").style.left = "20%";
+    document.getElementById("menu-loja").style.height = "10%";
+    document.getElementById("menu-loja").style.top = "0%";
+    
+    document.getElementById("menu-planos").style.left = "30%";
+    document.getElementById("menu-planos").style.height = "10%";
+    document.getElementById("menu-planos").style.top = "0%";
+    
+    document.getElementById("menu-cadastro").style.left = "43%";
+    document.getElementById("menu-cadastro").style.height = "10%";
+    document.getElementById("menu-cadastro").style.top = "0%";
+    document.getElementById("menu-cadastro").style.text = "0%";
+    
+    document.getElementById("menu-parceiros").style.left = "60%";
+    document.getElementById("menu-parceiros").style.height = "10%";
+    document.getElementById("menu-parceiros").style.top = "0%";
+    document.getElementById("menu-parceiros").style.text = "0%";
+    
+    document.getElementById("menu-contato").style.right = "10%";
+    document.getElementById("menu-contato").style.height = "10%";
+    document.getElementById("menu-contato").style.top = "0%";
+    document.getElementById("menu-contato").style.text = "0%";
+    
+    
+    //quando o scroll volta para o topo
     if (window.scrollY < 101)
     {
     document.getElementById("head").style.height = "20%";
@@ -21,12 +58,102 @@ function barraMenu()
     document.getElementById("head-vermelho").style.height = "10%";
     document.getElementById("head-branco").style.height = "12%";
     document.getElementById("nome").style.left = "30%";
+    //document.getElementById("nome").style.fontSize = "60px";
     document.getElementById("nome").style.fontSize = "60px";
-    document.getElementById("nome").style.fontSize = "60px";
+    document.getElementById("nome").style.height = "10%";
+    
+    document.getElementById("menu").style.height = "6%";
+    document.getElementById("menu").style.top = "14%";
+    document.getElementById("menu").style.width = "70%";
+    document.getElementById("menu").style.left = "28%";    
+        
+        document.getElementById("menu-login").style = "menu-login";
+        document.getElementById("menu-loja").style = "menu-loja";
+        document.getElementById("menu-planos").style = "menu-planos";
+        document.getElementById("menu-cadastro").style = "menu-cadastro";
+        document.getElementById("menu-parceiros").style = "menu-parceiros";
+        document.getElementById("menu-contato").style = "menu-contato";
         
     }
     
-//    document.getElementById("corpo").scrollTop = "50";
+
+}
+
+function menu(){
+
+//    document.getElementById("menu-login").style.color = "darkgrey";
+//    document.getElementById("menu-loja").style.color = "darkgrey";
+//    document.getElementById("menu-planos").style.color = "darkgrey";
+//    document.getElementById("menu-cadastro").style.color = "darkgrey";
+//    document.getElementById("menu-parceiros").style.color = "darkgrey";
+//    document.getElementById("menu-contato").style.color = "darkgrey";
+//    document.getElementById("menu").style.opacity = 0.5;
+    document.getElementById("menu").style.zIndex = 500;
     
-//    if (document.getElementById("corpo").scrollTop)
+}
+
+function menuLogin(){
+//    document.getElementById("menu-login").style.backgroundColor = "hotpink";
+//    document.getElementById("menu-login").style.opacity = 0.5;
+    document.getElementById("menu-login").style.zIndex = 800;
+    
+    document.getElementById("menu-loja").style.opacity = 0.3;
+    document.getElementById("menu-planos").style.opacity = 0.3;
+    document.getElementById("menu-cadastro").style.opacity = 0.3;
+    document.getElementById("menu-parceiros").style.opacity = 0.3;
+    document.getElementById("menu-contato").style.opacity = 0.3;
+    
+}
+
+function menuLoja(){
+//    document.getElementById("menu-loja").style.backgroundColor = "hotpink";  
+//    document.getElementById("menu-loja").style.color = "white";
+    
+    document.getElementById("menu-login").style.opacity = 0.3;
+    document.getElementById("menu-planos").style.opacity = 0.3;
+    document.getElementById("menu-cadastro").style.opacity = 0.3;
+    document.getElementById("menu-parceiros").style.opacity = 0.3;
+    document.getElementById("menu-contato").style.opacity = 0.3;
+}
+
+function menuPlanos(){
+    document.getElementById("menu-login").style.opacity = 0.3;
+    document.getElementById("menu-loja").style.opacity = 0.3;
+    document.getElementById("menu-cadastro").style.opacity = 0.3;
+    document.getElementById("menu-parceiros").style.opacity = 0.3;
+    document.getElementById("menu-contato").style.opacity = 0.3;
+}
+
+function menuCadastro(){
+    document.getElementById("menu-login").style.opacity = 0.3;
+    document.getElementById("menu-planos").style.opacity = 0.3;
+    document.getElementById("menu-loja").style.opacity = 0.3;
+    document.getElementById("menu-parceiros").style.opacity = 0.3;
+    document.getElementById("menu-contato").style.opacity = 0.3;
+}
+
+function menuParceiros(){
+    document.getElementById("menu-login").style.opacity = 0.3;
+    document.getElementById("menu-planos").style.opacity = 0.3;
+    document.getElementById("menu-cadastro").style.opacity = 0.3;
+    document.getElementById("menu-loja").style.opacity = 0.3;
+    document.getElementById("menu-contato").style.opacity = 0.3;
+}
+
+function menuContato(){
+    document.getElementById("menu-login").style.opacity = 0.3;
+    document.getElementById("menu-planos").style.opacity = 0.3;
+    document.getElementById("menu-loja").style.opacity = 0.3;
+    document.getElementById("menu-parceiros").style.opacity = 0.3;
+    document.getElementById("menu-cadastro").style.opacity = 0.3;
+}
+
+function foramenu(){
+    document.getElementById("menu-login").style.opacity = 1;
+    document.getElementById("menu-loja").style.opacity = 1;
+    document.getElementById("menu-planos").style.opacity = 1;
+    document.getElementById("menu-cadastro").style.opacity = 1;
+    document.getElementById("menu-parceiros").style.opacity = 1;
+    document.getElementById("menu-contato").style.opacity = 1;
+    document.getElementById("menu").style.opacity = 1;
 }
--- home.php
+++ home.php
@@ -4,20 +4,42 @@
     <head>
         <link rel="stylesheet" type="text/css" href="home.css">
         <script type="text/javascript" src="home.js"></script>
-        <div id="head">
+        
+        
+        
+        
+
+    </head>
+    <body onscroll="barraMenu()">
+        <div id="fundo">
+            <div id="rodape"><div id="logo-rodape"></div></div>
+        </div>
+        <div id="tudo">
+        <div id="pic-time"></div> <div id="img-comeco"></div>
+    </div>
+
+
+
+
+     <div id="head">
         <div id="head-branco">
             <div id="head-vermelho">
-                <div id="nome"> Porussia Dortmund
+                <div id="nome">
                 </div>
+            </div>
+            <div id="menu" onmouseover="menu()" onmouseout="foramenu()">
+                <div id="menu-login" onmouseover="menuLogin()">LOGIN</div>
+                <div id="menu-loja" onmouseover="menuLoja()">LOJA</div>
+                <div id="menu-planos" onmouseover="menuPlanos()">PLANOS</div>
+                <div id="menu-cadastro" onmouseover="menuCadastro()">CADASTRO</div>
+                <div id="menu-parceiros" onmouseover="menuParceiros()">PARCEIROS</div>
+                <div id="menu-contato" onmouseover="menuContato()">CONTATO</div>
             </div>
             </div></div>
         <div id="logo"></div>
-        
-        
-        <center><div id="corpo"></div></center>
-    </head>
-    <body onscroll="barraMenu()">
-        
+
+
+
     </body>
     
 </html>
